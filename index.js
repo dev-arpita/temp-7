dc7c5fe37888eb27d765733c74a671777935d769
Arpita Roy <arpita.roy.workar@gmail.com>
2026-02-17 22:21:12 +0530

    add dropdown functionality and update footer layout

---
+++ index.js
@@ -0,0 +1,13 @@
+// const dropdowns = document.querySelectorAll(".dropdown-btn");
+
+// dropdowns.forEach(btn => {
+//     const menu = btn.parentElement.nextElementSibling;
+
+//     btn.parentElement.addEventListener("mouseenter", () => {
+//         menu.classList.remove("hidden");
+//     });
+
+//     btn.parentElement.addEventListener("mouseleave", () => {
+//         menu.classList.add("hidden");
+//     });
+// });
